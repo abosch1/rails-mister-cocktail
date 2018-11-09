b591b9b9c120705d9b449af8a558a8f69c29650e
Astrid Bosch <boschschwarz@hotmail.com>
2018-11-09 10:54:11 -0300

    comparing and making changes according to the solution

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,36 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+puts "Starting seeding process..."
+
+puts "Destroy all Ingredients and Cocktails"
+Dose.destroy_all
+Cocktail.destroy_all
+Ingredient.destroy_all
+
+puts "Create new seeds!"
+
+Ingredient.create(name: "lemon")
+Ingredient.create(name: "ice")
+Ingredient.create(name: "mint leaves")
+Ingredient.create(name: "strawberry")
+Ingredient.create(name: "apple")
+Ingredient.create(name: "rum")
+Ingredient.create(name: "whiskey")
+Ingredient.create(name: "ice cubes")
+
+20.times do
+  cocktail = Cocktail.create(
+    name: Faker::Pokemon.name
+  )
+ 3.times do
+    Dose.create(
+      description: Faker::Food.measurement,
+      ingredient: Ingredient.last,
+      ingredient: Ingredient.first,
+      cocktail: cocktail
+    )
+  end
+end
+
+puts "Finished seeding process!"
