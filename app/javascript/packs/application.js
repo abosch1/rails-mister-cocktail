dba9e545d2406189983b7cd7729dcee2ba3ac454
Astrid Bosch <boschschwarz@hotmail.com>
2018-11-09 13:18:04 -0300

    fix bug

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -7,6 +7,6 @@
 // To reference this file, add <%= javascript_pack_tag 'application' %> to the appropriate
 // layout file, like app/views/layouts/application.html.erb
 import 'bootstrap';
-import improveDropdown from '../components/select';
+// import improveDropdown from '../components/select';
 
-improveDropdown();
+// improveDropdown();
